8f44f52996639b338ab0464240d35e2970c7ddaf
John Whitmore <johnfwhitmore@gmail.com>
2019-04-16 17:20:13 +0100

    Added media and printscreen keys
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- keyboards/satan/keymaps/jwhitmore/keymap.c
+++ keyboards/satan/keymaps/jwhitmore/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,
     OSL(CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS,    KC_ENT,
     KC_LSPO,   KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,              KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC,
-    KC_LCTL,   KC_LGUI, KC_LALT,                   LT(SPACE, KC_SPC),                                     OSL(FN1), KC_RALT, TG(NUMPAD), KC_RCTL
+    KC_LCTL,   KC_LGUI, KC_LALT,                   KC_SPC,                                     OSL(FN1), KC_RALT, TG(NUMPAD), KC_RCTL
   ),
 
   [SPACE] = LAYOUT_60_iso(
@@ -116,8 +116,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [FN1] = LAYOUT_60_iso( //Functions/settings
     KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,       KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, BL_TOGG, BL_INC,  BL_DEC,  BL_TOGG, _______, _______, _______,
+    _______, _______, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______,
+    _______, KC_MSTP, KC_VOLD, KC_MPLY, _______, _______, _______, BL_TOGG, BL_INC,  BL_DEC,  BL_TOGG, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______,                   _______,                                     _______, _______, _______, _______
   ),
